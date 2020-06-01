68e18175e513494a044751a08017dc1968e615bf
Bohdan Babchuk <babchuk.bohdan@gmail.com>
2020-06-01 18:44:40 +0300

    Initial

---
+++ routes/auth.routes.js
@@ -0,0 +1,95 @@
+const {Router} = require('express')
+const bcryptjs = require('bcryptjs');
+const config = require('config');
+const {check, validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = Router()
+
+// /api/auth/register
+router.post(
+  '/register',
+  [
+    check('email', 'Invalid email').isEmail(),
+    check('password', 'Password min lenght must be 6')
+      .isLength({min: 6})
+  ],
+  async (req, res) => {
+  try {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Invalid data in registration'
+      })
+    }
+
+    const {email, password} = req.body
+
+    const candidate = await User.findOne({email})
+
+    if (candidate) {
+      res.status(400).json({message: "This user is already exist"})
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 12)
+    const user = new User({email, password: hashedPassword})
+
+    await user.save()
+
+    res.status(201).json({message: 'User created'})
+
+  } catch (e) {
+    res.status(500).json({message: 'Something gone wrong, try again later'})
+  }
+})
+
+// /api/auth/login
+router.post(
+  '/login',
+  [
+    check('email', 'Enter correct email').normalizeEmail().isEmail(),
+    check('password', 'Enter password').exists()
+  ],
+  async (req, res) => {
+  try {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Invalid data in login system'
+      })
+    }
+
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    if (!user) {
+      return res.status(400).json({message: 'User doesn\'t exist'})
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: 'Wrong password, enter correct password'
+      })
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      config.get('jwtSecret'),
+      { expiresIn: '1h' }
+    )
+
+    res.json({ token, userId: user.id})
+
+  } catch (e) {
+    res.status(500).json({message: 'Something gone wrong, try again later'})
+  }
+})
+
+module.exports = router
